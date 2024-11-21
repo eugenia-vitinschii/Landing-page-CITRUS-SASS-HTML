452337a9e413ee72dc37e3992cea1697903b91a6
eugenia-vitinschii <vitinschii.eugenia@gmail.com>
2024-11-21 14:57:22 +0200

    create menu burger js, add new images and styles

--- js/index.js
+++ js/index.js
@@ -1,3 +1,9 @@
-function getMyFunction(){
-   alert("Hi!")
-}
+//burger menu
+const burgerButton = document.querySelector(".burger");
+const nav = document.querySelector(".header__nav");
+
+burgerButton.addEventListener('click', function () {
+  burgerButton.classList.toggle('active');
+  nav.classList.toggle('active');
+});
+ 
